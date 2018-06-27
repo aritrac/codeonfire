d26ec204e07b5f57e0e933ea99eb223dacc8a876
aritrac <aritrachatterjee2007@gmail.com>
2018-06-27 19:58:13 +0530

    Adding LL traversal problem

---
+++ 002_LinkedLists/src/ds/soln/aritra/FindNthNodeFromTheEndBruteForce.java
@@ -0,0 +1,15 @@
+package ds.soln.aritra;
+
+/*
+ * Author: Aritra Chatterjee
+ * Date: 25/06/2018
+ * Description: This implementation shows how to print the Nth node from the end of the linked list
+ */
+
+public class FindNthNodeFromTheEndBruteForce {
+	public static void main(String[] args) {
+		LinkedList ll = new LinkedList();
+	}
+	
+	
+}
