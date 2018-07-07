4201d5c4f26e03a43fa48920fddd7c7b100985a0
aritrac <aritrachatterjee2007@gmail.com>
2018-07-07 13:39:16 +0530

    Refactored code

--- 002_LinkedLists/src/ds/soln/aritra/SkipListTest.java
+++ 002_LinkedLists/src/ds/soln/aritra/SkipListTest.java
@@ -1,7 +1,9 @@
 package ds.soln.aritra;
 
 public class SkipListTest {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
+		@SuppressWarnings("rawtypes")
 		SkipList s = new SkipList();
 		s.add(1, 100);
 		System.out.println(s.get(1));
